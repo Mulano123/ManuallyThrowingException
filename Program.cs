fdf2d71b7b9dacb2e25cf37763462000d5d1be6f
mulano123 <joshuamulano26@gmail.com>
2023-10-22 11:36:31 +0800

    final

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ManuallyThrowingException
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmAddProduct());
+            Application.Run(new frmAddProduct());//
         }
     }
 }
