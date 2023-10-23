9cb5ae99261f101b70cd0904f57488114a792d16
mulano123 <joshuamulano26@gmail.com>
2023-10-23 23:15:22 +0800

    final changes in inventory

--- Inventory.cs
+++ Inventory.cs
@@ -21,9 +21,8 @@ namespace ManuallyThrowingException
         private string _Description;
         private int _Quantity;
         private double _SellingPrice;
-        private BindingSource showProductList;
-  
 
+  
         public frmAddProduct()
         {
             InitializeComponent();
@@ -53,52 +52,61 @@ namespace ManuallyThrowingException
         public string Product_Name(string name)
         {
             if (!Regex.IsMatch(name, @"^[a-zA-Z ]+$"))
-                throw new StringFormatException("Invalid Input!! String Format Exception.");
+                throw new StringFormatException("Invalid Input!! Please enter the Product Name.");
             return name;
         }
         public int Quantity(string qty)
         {
             if (!Regex.IsMatch(qty, @"^[0-9]"))
-                throw new NumberFormatException("Invalid Input!! Number Format Exception.");
+                throw new NumberFormatException("Invalid Input!! Please enter the Quantity.");
             return Convert.ToInt32(qty);
         }
 
         public double SellingPrice(string price)
         {
             if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
-                throw new CurrencyFormatException("Invalid Input!! Currency Format Exception.");
+                throw new CurrencyFormatException("Invalid Input!! Please enter the Selling Price.");
             return Convert.ToDouble(price);
         }
 
         class NumberFormatException : Exception
         {
-            public NumberFormatException(string numberException) : base(numberException) { }
+            public NumberFormatException(string numberExcept) : base(numberExcept) { }
         }
 
         class StringFormatException : Exception
         {
-            public StringFormatException(string stringException) : base(stringException) { }
+            public StringFormatException(string stringExcept) : base(stringExcept) { }
         }
 
         class CurrencyFormatException : Exception
         {
-            public CurrencyFormatException(string currencyException) : base(currencyException) { }
+            public CurrencyFormatException(string currencyExcept) : base(currencyExcept) { }
         }
 
         private void btnAddProduct_Click(object sender, EventArgs e)
         {
             try
             {
-                _ProductName = Product_Name(txtProductName.Text);
-                _Category = cbCategory.Text;
-                _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
-                _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
-                _Description = richTxtDescription.Text;
-                _Quantity = Quantity(txtQuantity.Text);
-                _SellingPrice = SellingPrice(txtSellPrice.Text);
-                showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate, _ExpDate, _SellingPrice, _Quantity, _Description));
-                gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-                gridViewProductList.DataSource = showProductList;
+                if (string.IsNullOrEmpty(txtProductName.Text) || (string.IsNullOrEmpty(txtQuantity.Text) || 
+                    (string.IsNullOrEmpty(txtSellPrice.Text) || (string.IsNullOrEmpty(richTxtDescription.Text) ||
+                    (cbCategory.SelectedIndex == -1)))))
+                {
+                    MessageBox.Show("Please fill up the form to add product!!");
+                }
+                else
+                {
+                    _ProductName = Product_Name(txtProductName.Text);
+                    _Category = cbCategory.Text;
+                    _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
+                    _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+                    _Description = richTxtDescription.Text;
+                    _Quantity = Quantity(txtQuantity.Text);
+                    _SellingPrice = SellingPrice(txtSellPrice.Text);
+                    showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate, _ExpDate, _SellingPrice, _Quantity, _Description));
+                    gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    gridViewProductList.DataSource = showProductList;
+                }
             }
             catch (StringFormatException sfe)
             {
@@ -110,12 +118,10 @@ namespace ManuallyThrowingException
             }
             catch (CurrencyFormatException cfe)
             {
-                MessageBox.Show("Message: " +cfe.Message);
+                MessageBox.Show("Message: " + cfe.Message);
             }
 
         }
     }
 }
-
-
 
